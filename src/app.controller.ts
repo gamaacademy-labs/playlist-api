528ffc5f1c59f933561b18c1de7665748e741097
robkbr <roberto.junior@nickelpay.com.br>
2022-08-09 19:36:54 -0300

    chore: set app controller tag

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiTags('status')
   @Get()
   getHello() {
     return this.appService.getHello();
