20e9acb15573c73e9b072449011be4db5e3ed001
robkbr <roberto.junior@nickelpay.com.br>
2022-08-17 12:50:19 -0300

    chore: remove unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './infrastructure/database/database.module';
-import { PlaylistsModule } from './infrastructure/modules/playlists/playlists.module';
 import { ContentsModule } from './infrastructure/modules/contents/contents.module';
 
 @Module({
-  imports: [DatabaseModule, PlaylistsModule, ContentsModule],
+  imports: [DatabaseModule, ContentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
