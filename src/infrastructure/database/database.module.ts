d0e07aa6fdafae2afc20eedfd4af6f1d651d67dc
robkbr <roberto.junior@nickelpay.com.br>
2022-08-17 12:47:48 -0300

    refactor: move playlist data methods to inside repositories

--- src/infrastructure/database/database.module.ts
+++ src/infrastructure/database/database.module.ts
@@ -1,9 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaProvider } from './providers/prisma.provider';
+import { PlaylistsRepository } from './repositories/playlists.repository';
 
+// Esse decorator esta sendo usado para que tudo que ta
+// aqui dentro possa ser usado em qualquer lugar
 @Global()
 @Module({
-  providers: [PrismaProvider],
+  providers: [PrismaProvider, PlaylistsRepository],
   exports: [PrismaProvider],
 })
 export class DatabaseModule {}
