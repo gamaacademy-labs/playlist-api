90fee280cdac617b10a2c9a76887ca07e649b4ba
sottjr <sottjr@gmail.com>
2022-08-18 03:47:53 +0200

    refactor: add ContentsRepository in providers @global

--- src/infrastructure/database/database.module.ts
+++ src/infrastructure/database/database.module.ts
@@ -1,12 +1,13 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaProvider } from './providers/prisma.provider';
+import { ContentsRepository } from './repositories/contents.repository';
 import { PlaylistsRepository } from './repositories/playlists.repository';
 
 // Esse decorator esta sendo usado para que tudo que ta
 // aqui dentro possa ser usado em qualquer lugar
 @Global()
 @Module({
-  providers: [PrismaProvider, PlaylistsRepository],
+  providers: [PrismaProvider, PlaylistsRepository, ContentsRepository],
   exports: [PrismaProvider],
 })
 export class DatabaseModule {}
