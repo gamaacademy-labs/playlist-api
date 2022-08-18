a54f076c6a97e7aceb909fd1e173bfea2572ac99
sottjr <sottjr@gmail.com>
2022-08-18 04:03:11 +0200

    refactor: delete import contentsmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './infrastructure/database/database.module';
-import { ContentsModule } from './infrastructure/modules/contents/contents.module';
 
 @Module({
-  imports: [DatabaseModule, ContentsModule],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
