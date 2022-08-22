5772b22469666d7a6826518c706f8e0534a67602
sottjr <sottjr@gmail.com>
2022-08-23 00:04:17 +0200

    chore: link studentsHistories repository on providers

--- src/infrastructure/database/database.module.ts
+++ src/infrastructure/database/database.module.ts
@@ -3,12 +3,18 @@ import { Global, Module } from '@nestjs/common';
 import { PrismaProvider } from './providers/prisma.provider';
 import { ContentsRepository } from './repositories/contents.repository';
 import { PlaylistsRepository } from './repositories/playlists.repository';
+import { StudentsHistoriesRepository } from './repositories/studentsHistories.repository';
 
 // Esse decorator esta sendo usado para que tudo que ta
 // aqui dentro possa ser usado em qualquer lugar
 @Global()
 @Module({
-  providers: [PrismaProvider, PlaylistsRepository, ContentsRepository],
+  providers: [
+    PrismaProvider,
+    PlaylistsRepository,
+    ContentsRepository,
+    StudentsHistoriesRepository,
+  ],
   exports: [PrismaProvider],
 })
 export class DatabaseModule {}
