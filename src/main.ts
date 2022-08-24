6799cb145298e98032771fefd6f23778f3be14f0
robkbr <roberto.junior@nickelpay.com.br>
2022-08-24 18:08:07 -0300

    feat: add enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { swaggerConfig } from './infrastructure/configs/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   // API Documentation
   const document = SwaggerModule.createDocument(app, swaggerConfig);
