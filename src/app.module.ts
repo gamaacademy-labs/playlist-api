cfdefd18e59988d5f296a4b895ed59ba7c09e5ef
robkbr <roberto.junior@nickelpay.com.br>
2022-08-24 12:10:40 -0300

    feat: register controller module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './infrastructure/database/database.module';
+import { HttpModule } from './infrastructure/http/http.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
