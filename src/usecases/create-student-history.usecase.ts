f45468cbf1fc0f9ada737ea29db6abd1567c9746
robkbr <roberto.junior@nickelpay.com.br>
2022-08-25 13:09:52 -0300

    refactor: remove unused cases and update code

--- src/usecases/create-student-history.usecase.ts
+++ src/usecases/create-student-history.usecase.ts
@@ -1,7 +1,7 @@
 import { StudentsHistoriesDTO } from 'src/domain/dto/studentsHistories.dto';
 import { StudentsHistoriesRepository } from 'src/infrastructure/database/repositories/studentsHistories.repository';
 
-export class CreateStudentsHistorieUsecase {
+export class CreateStudentsHistoryUsecase {
   constructor(
     private readonly StudentsHistoriesRepository: StudentsHistoriesRepository,
   ) {}
