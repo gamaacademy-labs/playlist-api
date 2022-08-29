ed22e21007ed2f1149dc8e1f33ad3c545ce4cc2e
sottjr <sottjr@gmail.com>
2022-08-29 22:05:20 +0200

    chore: export StudentsPlaylistsFavoritesRepository provider on database.modules.ts

--- src/infrastructure/database/database.module.ts
+++ src/infrastructure/database/database.module.ts
@@ -2,13 +2,24 @@ import { Global, Module } from '@nestjs/common';
 
 import { PrismaProvider } from './providers/prisma.provider';
 import { PlaylistsRepository } from './repositories/playlists.repository';
+import { StudentsPlaylistsFavoritesRepository } from './repositories/studentPlaylistsFavorites.repository';
 import { StudentsHistoriesRepository } from './repositories/studentsHistories.repository';
 
 // Esse decorator esta sendo usado para que tudo que ta
 // aqui dentro possa ser usado em qualquer lugar
 @Global()
 @Module({
-  providers: [PrismaProvider, PlaylistsRepository, StudentsHistoriesRepository],
-  exports: [PrismaProvider, PlaylistsRepository, StudentsHistoriesRepository],
+  providers: [
+    PrismaProvider,
+    PlaylistsRepository,
+    StudentsHistoriesRepository,
+    StudentsPlaylistsFavoritesRepository,
+  ],
+  exports: [
+    PrismaProvider,
+    PlaylistsRepository,
+    StudentsHistoriesRepository,
+    StudentsPlaylistsFavoritesRepository,
+  ],
 })
 export class DatabaseModule {}
