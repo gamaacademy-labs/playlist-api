06d01159d99c7d8ca50f78eb56a1e00abfad809b
sottjr <sottjr@gmail.com>
2022-08-29 22:15:30 +0200

    feat: create usecase create-students-playlists-favorites

---
+++ src/usecases/create-student-playlists-favorites.usecase.ts
@@ -0,0 +1,14 @@
+import { StudentsPlaylistsFavoritesDTO } from 'src/domain/dto/studentPlaylistsFavorites.dto';
+import { StudentsPlaylistsFavoritesRepository } from 'src/infrastructure/database/repositories/studentPlaylistsFavorites.repository';
+
+export class CreateStudentsPlaylistsFavorites {
+  constructor(
+    private readonly studentsPlaylistsFavoritesRepository: StudentsPlaylistsFavoritesRepository,
+  ) {}
+
+  async create(data: StudentsPlaylistsFavoritesDTO) {
+    const studentPlaylistsFavorites =
+      await this.studentsPlaylistsFavoritesRepository.create(data);
+    return studentPlaylistsFavorites;
+  }
+}
