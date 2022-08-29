f42057f10395feeb5e537fce44d05817cbb174cb
sottjr <sottjr@gmail.com>
2022-08-29 21:56:12 +0200

    feat: create studentPlaylistsFavoritesDTO

---
+++ src/domain/dto/studentPlaylistsFavorites.dto.ts
@@ -0,0 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class StudentsPlaylistsFavoritesDTO {
+  @ApiProperty()
+  id: string;
+  @ApiProperty()
+  playlistId: string;
+  @ApiProperty()
+  studentId: string;
+}
