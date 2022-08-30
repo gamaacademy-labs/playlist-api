59b0d06a796a27149722712e28f728bb962e521f
andradeju <julliana.andrade08@gmail.com>
2022-08-30 19:05:58 -0300

    feat: additional contents

--- prisma/migrations/20220830215509_additional_contents/migration.sql
+++ prisma/migrations/20220830215509_additional_contents/migration.sql
@@ -72,13 +72,24 @@ CREATE TABLE `students` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `students_ßhistories` (
+CREATE TABLE `students_histories` (
     `id` VARCHAR(191) NOT NULL,
     `contentId` VARCHAR(191) NOT NULL,
     `studentId` VARCHAR(191) NOT NULL,
     `isVideoFinished` BOOLEAN NOT NULL DEFAULT false,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `additional_contents` (
+    `id` VARCHAR(191) NOT NULL,
+    `contentId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `type` ENUM('VIDEO', 'SLIDE', 'PDF') NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
